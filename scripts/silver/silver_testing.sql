87a9f6a21bcf29bf2d6b24ad592fcc8eb8fdd904
TravisArnt <tinthtoo008@gmail.com>
2026-06-19 18:27:52 +0700

    merge into prod file

--- scripts/silver/silver_testing.sql
+++ scripts/silver/silver_testing.sql
@@ -20,3 +20,6 @@ WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info)
 SELECT * 
 FROm silver.crm_sales_details
 WHERE sls_sales IS NULL OR sls_sales != sls_quantity * sls_price 
+
+SELECT DISTINCT(cntry)
+FROM silver.erp_loc_a101
